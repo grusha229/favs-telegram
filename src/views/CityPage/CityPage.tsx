f74392bfed73d843de0f6f2fdac8f80802c0efd2
Grigory Tupikov <gtupikov@jetlend.ru>
2024-05-22 00:10:21 +0300

    upd

--- src/views/CityPage/CityPage.tsx
+++ src/views/CityPage/CityPage.tsx
@@ -8,6 +8,7 @@ import { IPlaceApiResponse } from '../../models/Places'
 export default function CityPage() {
     // console.log('user ',window.navigator?.userAgentData?.platform);
     const [coordinates, setCoordinates] = useState<Partial<GeolocationCoordinates>>(null)
+    const [error, setError] = useState<string>(null)
     const [places, setPlaces] = useState<IPlaceApiResponse[]>(null)
     const USER_TOKEN = useSelector((state: IStateInterface) => state.authentication.token)
     const USER_ID = useSelector((state: IStateInterface) => state.authentication.telegramID)
@@ -27,24 +28,36 @@ export default function CityPage() {
             console.log(places)
             setPlaces(places)
         })
+        .catch((e) => {
+            console.error('errorr!', e)
+            setError(`${e?.response?.status}, ${e?.response?.statusText}`)
+        })
     },[USER_TOKEN, USER_ID, coordinates])
 
   return (
     <div>
         <div className='Container'>
             <h1>City page</h1>
-            <div className='Card'>
-                {coordinates?.latitude}
-            </div>
-            <div className='Card'>
-                {coordinates?.longitude}
-            </div>
-            {
-                places?.map((place) => (
+            { coordinates &&
+                <>
+                    <div className='Card'>
+                        {coordinates?.latitude}
+                    </div>
+                    <div className='Card'>
+                        {coordinates?.longitude}
+                    </div>
+                </>
+            }
+            {places?.map((place) => (
                     <div className='Card'>
                         {place?.name}
                     </div>
                 ))
+            }
+            {error && 
+                    <div className='Card'>
+                            {error}
+                    </div>
             }
         </div>
     </div>
