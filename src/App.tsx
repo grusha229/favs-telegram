2fd50ecf55168204454a20b0d75b38dbd85b09e4
Grigory Tupikov <gtupikov@jetlend.ru>
2024-06-07 00:55:47 +0300

    upd provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import '@twa-dev/sdk';
 import { Outlet } from 'react-router-dom';
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import { AppRoot } from '@telegram-apps/telegram-ui';
+import { SDKProvider } from '@tma.js/sdk-react';
 
 function App() {
 
   return (
     <AppRoot>
-      <div className='App'>
-        <Outlet />
-      </div>
+      <SDKProvider acceptCustomStyles debug>
+        <div className='App'>
+          <Outlet />
+        </div>
+      </SDKProvider>
     </AppRoot>
   );
 }
